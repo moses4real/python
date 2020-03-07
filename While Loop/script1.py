53e9a35fbf7554bde7a5422414085f6a4e14a0b2
moses4real <61254175+moses4real@users.noreply.github.com>
2020-03-07 22:03:56 +0100

    Add files via upload

---
+++ While Loop/script1.py
@@ -0,0 +1,8 @@
+Password= ''
+
+while password != 'Python123':
+    password = input("Enter password: ")
+    if password == 'python123':
+        print("you are logged in! ")
+    else:
+        print("sorry, try again! ")
